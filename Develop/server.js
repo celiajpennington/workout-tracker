d876e78941376af5604543128d232e89264a554a
Celia Pennington <celiajpennington@gmail.com>
2021-08-18 16:53:05 -0500

    changed server.js

--- Develop/server.js
+++ Develop/server.js
@@ -7,14 +7,19 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true
-});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false
+    }
+);
+
 // routes
 app.use(require("./routes/apiRoutes.js"));
 app.use(require("./routes/views.js"));
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+    console.log(`App running on port ${PORT}!`);
 });
